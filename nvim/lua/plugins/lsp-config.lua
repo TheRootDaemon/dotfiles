64643ff60464d22a43cec409d1bdfde388a74fde
TheRootDaemon <harshath3018@gmail.com>
2025-06-30 23:04:43 +0530

    ts_ls

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,7 +15,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "pyright",
-                "tsserver",
+                "ts_ls",
                 "html",
                 "cssls",
                 "jsonls",
