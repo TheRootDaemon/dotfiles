2b6ef5c7742c270e994635b3c4351a2b3516c283
TheRootDaemon <harshath3018@gmail.com>
2025-07-09 21:55:27 +0530

    feat: Format on save, in lsp-config

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -13,7 +13,9 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.autoindent = true
 vim.opt.smarttab = true
+vim.opt.smartindent = true
 
 -- Search configs
 vim.opt.hlsearch = false
