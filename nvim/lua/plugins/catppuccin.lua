09b8c15b2ea82ec26ede01226cb5eeed7412e0d5
TheRootDaemon <harshath3018@gmail.com>
2025-07-14 14:44:59 +0530

    feat: transparent background

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,17 +1,18 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            custom_highlights = function(colors)
-                return {
-                    LineNr = { fg = colors.flamingo },
-                }
-            end,
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+			flavour = "mocha",
+			custom_highlights = function(colors)
+				return {
+					LineNr = { fg = colors.flamingo },
+				}
+			end,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
