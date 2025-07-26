78315344334df3985d3ff79740e20b6b85569240
TheRootDaemon <harshath3018@gmail.com>
2025-07-26 10:42:48 +0530

    reset: lsp-config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -10,6 +10,7 @@ return {
         local mason_lspconfig = require("mason-lspconfig")
 
         mason.setup()
+
         mason_lspconfig.setup({
             ensure_installed = {
                 "lua_ls",
@@ -66,6 +67,7 @@ return {
 
         local on_attach = function(_, bufnr)
             local opts = { buffer = bufnr, silent = true, noremap = true }
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover" }))
             vim.keymap.set(
                 "n",
@@ -82,66 +84,76 @@ return {
             vim.keymap.set("n", "ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
         end
 
-        -- Server configurations
         local servers = {
-            lua_ls = {
-                settings = {
-                    Lua = {
-                        runtime = { version = "LuaJIT" },
-                        diagnostics = { globals = { "vim" } },
-                        workspace = { library = { vim.env.VIMRUNTIME } },
-                    },
-                },
-            },
-            cssls = {
-                filetypes = { "css", "scss", "svelte", "less" },
-                settings = {
-                    css = {
-                        validate = true,
-                        lint = {
-                            unknownAtRules = "ignore",
+            lua_ls = function()
+                lspconfig.lua_ls.setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = {
+                        Lua = {
+                            runtime = { version = "LuaJIT" },
+                            diagnostics = { globals = { "vim" } },
+                            workspace = { library = { vim.env.VIMRUNTIME } },
                         },
                     },
-                    scss = {
-                        validate = true,
-                        lint = {
-                            unknownAtRules = "ignore",
+                })
+            end,
+
+            cssls = function()
+                lspconfig.cssls.setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    filetypes = { "css", "scss", "svelte", "less" },
+                    settings = {
+                        css = {
+                            validate = true,
+                            lint = {
+                                unknownAtRules = "ignore",
+                            },
+                        },
+                        scss = {
+                            validate = true,
+                            lint = {
+                                unknownAtRules = "ignore",
+                            },
+                        },
+                        less = {
+                            validate = true,
+                            lint = {
+                                unknownAtRules = "ignore",
+                            },
                         },
                     },
-                    less = {
-                        validate = true,
-                        lint = {
-                            unknownAtRules = "ignore",
-                        },
-                    },
-                },
-            },
-            svelte = {
-                on_attach = function(client, bufnr)
-                    on_attach(client, bufnr)
-                    if client.name == "svelte" then
-                        vim.api.nvim_create_autocmd("BufWritePost", {
-                            pattern = { "*.js", "*.ts", "*.svelte" },
-                            callback = function(ctx)
-                                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-                            end,
-                        })
-                    end
-                end,
-            },
+                })
+            end,
+
+            svelte = function()
+                lspconfig.svelte.setup({
+                    capabilities = capabilities,
+                    on_attach = function(client, bufnr)
+                        on_attach(client, bufnr)
+                        if client.name == "svelte" then
+                            vim.api.nvim_create_autocmd("BufWritePost", {
+                                pattern = { "*.js", "*.ts", "*.svelte" },
+                                callback = function(ctx)
+                                    client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+                                end,
+                            })
+                        end
+                    end,
+                })
+            end,
+
+            _default = function(server_name)
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end,
         }
 
-        -- Setup servers installed by mason-lspconfig
-        for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
-            local server_config = servers[server_name] or {}
-
-            -- Merge default config with server-specific config
-            local config = vim.tbl_deep_extend("force", {
-                capabilities = capabilities,
-                on_attach = on_attach,
-            }, server_config)
-
-            lspconfig[server_name].setup(config)
+        for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+            (servers[server] or servers._default)(server)
         end
     end,
 }
