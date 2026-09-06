6ad699de3f2dfcdeb8563ff5ba08ac378133b1e6
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-09-06 22:33:18 +0530

    refacotor: Aliases, starship prompt

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -1,15 +1,9 @@
-# Aliases
-alias vim="nvim"
-alias ff="fastfetch"
+alias v="nvim"
+alias x="tmux"
+
 alias ls="ls --color=auto"
 alias grep="grep --color=auto"
 
 alias nginx="sudo nginx"
 alias pacman="sudo pacman"
 alias systemctl="sudo systemctl"
-
-update() {
-    sudo pacman -Syu
-    yay -Syu
-    flatpak update
-}
