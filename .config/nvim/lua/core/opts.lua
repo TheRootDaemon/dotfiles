1d01aee6591f0cb13360b9245bf8d578489cfcad
TheRootDaemon <harsha.manjula.venkataramanan@gmail.com>
2026-09-13 17:09:51 +0530

    fix(nvim): Refactor conflicting keymaps with tmux

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -46,7 +46,7 @@ vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
 
 -- clear search highlight with <Esc>
 vim.keymap.set("n", "<Esc>", "<CMD>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>h", ":%s/")
+vim.keymap.set("n", "<leader>f", ":%s/")
 
 -- move selected lines up and down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -61,16 +61,16 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 -- motions for panes
-vim.keymap.set("n", "<M-h>", "<C-w>h")
-vim.keymap.set("n", "<M-j>", "<C-w>j")
-vim.keymap.set("n", "<M-k>", "<C-w>k")
-vim.keymap.set("n", "<M-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
 
 -- resize panes
-vim.keymap.set("n", "<M-H>", "<C-w>2>")
-vim.keymap.set("n", "<M-J>", "<C-w>2-")
-vim.keymap.set("n", "<M-K>", "<C-w>2+")
-vim.keymap.set("n", "<M-L>", "<C-w>2<")
+vim.keymap.set("n", "<leader>H", "<C-w>2>")
+vim.keymap.set("n", "<leader>J", "<C-w>2-")
+vim.keymap.set("n", "<leader>K", "<C-w>2+")
+vim.keymap.set("n", "<leader>L", "<C-w>2<")
 
 -- annoying motions
 vim.cmd([[
